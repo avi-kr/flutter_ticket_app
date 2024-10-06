0213b35fce93c9dd34eca6cb1d23a312e19dc201
Abhishek Kumar <avi.kr16@gmail.com>
2024-10-06 13:47:16 +0530

    Ticket App, initial home screen

---
+++ README.md
@@ -0,0 +1,4 @@
+# ticket_app
+
+A new Flutter project.
+
---
+++ android/app/src/main/kotlin/com/abhishek/ticket_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.abhishek.ticket_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
